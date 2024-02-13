a34cf20e7a46aaf09162172b1690b7384909f85b
Miner34dev <134534394+Miner34dev@users.noreply.github.com>
2024-02-13 14:00:23 +0100

    Separate dig actions from other ones
    Other similar stuff coming soon.
    The identification code isn't very good (some MT mods logs may be recognized as players digging stuff) and will be redone in the future.

--- mt_log_data_extractor.py
+++ mt_log_data_extractor.py
@@ -5,5 +5,11 @@ def import_mt_log(file_path):
     for x in input_data:
         if x[21:36]=="ACTION[Server]:":
             date = datetime(int(x[:4]), int(x[5:7]), int(x[8:10]), int(x[11:13]), int(x[14:16]), int(x[17:19]))
-            result.append({"when":date, "what":x[37:]})
+            data = x[37:]
+            datasplit = data.split()
+            if len(datasplit)==5:
+                act_type = {"who":datasplit[0], "action":datasplit[1], "node":datasplit[2], "position":datasplit[4]}
+            else:
+                act_type = data
+            result.append({"when":date, "what":act_type})
     return result
