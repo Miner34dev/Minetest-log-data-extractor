c2b343493fb1a9cd20840cd760d3e927986ceabe
Miner34dev <134534394+Miner34dev@users.noreply.github.com>
2024-02-13 19:17:59 +0100

    Fix the action selection code
    The other one was that bad that i decided to fix it immediately.

--- mt_log_data_extractor.py
+++ mt_log_data_extractor.py
@@ -7,7 +7,7 @@ def import_mt_log(file_path):
             date = datetime(int(x[:4]), int(x[5:7]), int(x[8:10]), int(x[11:13]), int(x[14:16]), int(x[17:19]))
             data = x[37:]
             datasplit = data.split()
-            if len(datasplit)==5:
+            if datasplit[1]=="digs":
                 act_type = {"who":datasplit[0], "action":datasplit[1], "node":datasplit[2], "position":datasplit[4]}
             else:
                 act_type = data
